03df6adc00de02de6b02f5f60bca45f31c9b8ba8
Illia Khrypunov <illia.khrypunov@gmail.com>
2022-12-28 09:54:10 +0200

    Added required fonts, implemented structure for controlling fonts from code

---
+++ iFoot3D/iFoot3D/Resources/Fonts/Font/Font.swift
@@ -0,0 +1,38 @@
+//
+//  Font.swift
+//  iFoot3D
+//
+//  Created by Illia Khrypunov on 28.12.2022.
+//
+
+import UIKit.UIFont
+
+enum Font {
+    private static let futuraBold: UIFont = UIFont(name: "FuturaBold", size: 10)!
+    private static let sfProTextBold: UIFont = UIFont(name: "SF-Pro-Text-Bold", size: 10)!
+    private static let sfProTextMedium: UIFont = UIFont(name: "SF-Pro-Text-Medium", size: 10)!
+    private static let sfProTextRegular: UIFont = UIFont(name: "SF-Pro-Text-Regular", size: 10)!
+    private static let sfProTextThin: UIFont = UIFont(name: "SF-Pro-Text-Thin", size: 10)!
+}
+
+extension Font {
+    static func futuraBold(_ size: CGFloat) -> UIFont {
+        return self.futuraBold.withSize(size)
+    }
+    
+    static func sfProTextBold(_ size: CGFloat) -> UIFont {
+        return self.sfProTextBold.withSize(size)
+    }
+    
+    static func sfProTextMedium(_ size: CGFloat) -> UIFont {
+        return self.sfProTextMedium.withSize(size)
+    }
+    
+    static func sfProTextRegular(_ size: CGFloat) -> UIFont {
+        return self.sfProTextRegular.withSize(size)
+    }
+    
+    static func sfProTextThin(_ size: CGFloat) -> UIFont {
+        return self.sfProTextThin.withSize(size)
+    }
+}
