b14a518e0b3a3e79b65092ead79ddcddafde4b1e
Illia Khrypunov <illia.khrypunov@gmail.com>
2023-02-21 09:58:26 +0200

    Objc wrappers implemented

---
+++ iFoot3D/iFoot3D/BusinessLogic/Services/Reconstruction/ReconstructionService/Wrapper/ObjectiveC/ReconstructionServiceWrapper.h
@@ -0,0 +1,25 @@
+//
+//  ReconstructionServiceWrapper.h
+//  iFoot3D
+//
+//  Created by Illia Khrypunov on 21.02.2023.
+//
+
+#ifndef ReconstructionServiceWrapper_h
+#define ReconstructionServiceWrapper_h
+
+#import <UIKit/UIKit.h>
+
+@interface ReconstructionServiceWrapper : NSObject
+// MARK: - Init
+- (nonnull instancetype) init;
+
+// MARK: - Reconstruction
+- (void) reconstruct: (nonnull NSArray<NSArray<NSString *> *> *) rightSide
+       leftSidePaths: (nonnull NSArray<NSArray<NSString *> *> *) leftSide
+           solePaths: (nonnull NSArray<NSArray<NSString *> *> *) sole
+          outputPath: (nonnull NSString *) output;
+
+@end
+
+#endif /* ReconstructionServiceWrapper_h */
---
+++ iFoot3D/iFoot3D/BusinessLogic/Services/Reconstruction/ReconstructionService/Wrapper/ObjectiveC/ReconstructionServiceWrapper.mm
@@ -0,0 +1,54 @@
+//
+//  ReconstructionServiceWrapper.mm
+//  iFoot3D
+//
+//  Created by Illia Khrypunov on 21.02.2023.
+//
+
+#import <Foundation/Foundation.h>
+#import <opencv2/core.hpp>
+#import "ReconstructionServiceWrapper.h"
+#import "io.h"
+#import "reconstruction.h"
+
+@implementation ReconstructionServiceWrapper
+// MARK: - Init
+- (instancetype) init {
+    self = [super init];
+    return self;
+}
+
+// MARK: - Reconstruction
+- (void) reconstruct: (NSArray<NSArray<NSString *> *> *) rightSide
+       leftSidePaths: (NSArray<NSArray<NSString *> *> *) leftSide
+           solePaths: (NSArray<NSArray<NSString *> *> *) sole
+          outputPath: (NSString *) output {
+    std::vector<std::vector<std::string>> rightPaths = [self getPaths:rightSide];
+    std::vector<std::vector<std::string>> leftPaths = [self getPaths:leftSide];
+    std::vector<std::vector<std::string>> solePaths = [self getPaths:sole];
+    
+    std::vector<std::vector<std::vector<cv::Mat>>> input = ifoot3d::readMultipleInputData(rightPaths,
+                                                                                          leftPaths,
+                                                                                          solePaths);
+    // call reconstruct leg function
+}
+
+// MARK: - Helpers
+- (std::vector<std::vector<std::string>>) getPaths: (NSArray<NSArray<NSString *> *> *) paths {
+    std::vector<std::vector<std::string>> output = {};
+    
+    for (NSArray<NSString *> * path in paths) {
+        std::vector<std::string> outputPaths = {};
+        
+        for (NSString * string in path) {
+            std::string path = [string cStringUsingEncoding: NSUTF8StringEncoding];
+            outputPaths.push_back(path);
+        }
+        
+        output.push_back(outputPaths);
+    }
+    
+    return output;
+}
+
+@end
